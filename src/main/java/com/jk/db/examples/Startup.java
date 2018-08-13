cd50fa0e3db6c781641b000877da0b9abc727d3a
Jalal Kiswani <kiswanij@gmail.com>
2018-08-13 01:13:46 -0700

    .

--- src/main/java/com/jk/db/examples/Startup.java
+++ src/main/java/com/jk/db/examples/Startup.java
@@ -1,13 +1,14 @@
 package com.jk.db.examples;
 
 import com.jk.application.api.ApplicationManager;
+import com.jk.application.api.ApplicationSuperFactory;
+import com.jk.desktop.ui.UIDesktopWidgetFactory;
 import com.jk.util.JK;
 
 public class Startup {
 	public static void main(String[] args) {
 		JK.debug();
-		ApplicationManager instance = ApplicationManager.getInstance();
-		instance.init();
-		instance.start();
+		ApplicationSuperFactory.getInstance().setUIWidgetFactorty(new UIDesktopWidgetFactory());
+		ApplicationManager.getInstance().start();
 	}
 }
