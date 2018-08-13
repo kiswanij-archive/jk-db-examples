acfaf5a57f87431e267ab3be7bc5f33712acc2e0
Jalal Kiswani <kiswanij@gmail.com>
2018-08-12 22:21:49 -0700

    .

--- src/main/java/com/jk/db/examples/Startup.java
+++ src/main/java/com/jk/db/examples/Startup.java
@@ -1,15 +1,13 @@
 package com.jk.db.examples;
 
-import com.jk.framework.application.ApplicationManager;
+import com.jk.application.api.ApplicationManager;
 import com.jk.util.JK;
 
 public class Startup {
 	public static void main(String[] args) {
 		JK.debug();
-		JK.printBlock("1");
-		ApplicationManager.getInstance().init();
-		JK.printBlock("2");
-		ApplicationManager.getInstance().start();
-		JK.printBlock("3");
+		ApplicationManager instance = ApplicationManager.getInstance();
+		instance.init();
+		instance.start();
 	}
 }
