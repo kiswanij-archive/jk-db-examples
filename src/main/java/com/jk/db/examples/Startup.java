0414bcf077b1d5a9713c4acd6ca9a587faf24ad4
Jalal Kiswani <kiswanij@gmail.com>
2018-08-20 14:18:44 -0700

    .

--- src/main/java/com/jk/db/examples/Startup.java
+++ src/main/java/com/jk/db/examples/Startup.java
@@ -1,18 +1,12 @@
 package com.jk.db.examples;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 
-import com.jk.desktop.JKDesktopApplication;
-
+import com.jk.application.api.JKApplicationManager;
 
 public class Startup {
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		JKDesktopApplication.start();
+		JKApplicationManager.getInstance().start();
 	}
 
 	
